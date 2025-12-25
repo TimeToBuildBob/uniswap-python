6a113d5f8522c6a2ae3690bbd58872d7fdb03a3e
Yohan K <72107640+liquid-8@users.noreply.github.com>
2025-12-25 18:49:39 +0200

    Add Uniswap4 to module exports

--- uniswap/__init__.py
+++ uniswap/__init__.py
@@ -3,4 +3,4 @@ from .cli import main
 from .uniswap import Uniswap, _str_to_addr
 from .uniswap4 import Uniswap4
 
-__all__ = ["Uniswap", "exceptions", "_str_to_addr", "main"]
+__all__ = ["Uniswap", "Uniswap4", "exceptions", "_str_to_addr", "main"]
